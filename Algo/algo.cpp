7fc9e1f5348134da67ad2f25cf95639a0a26b77f
arpit92k <arpit92k@gmail.com>
2015-05-29 09:07:33 +0530

    Update algo.cpp

--- Algo/algo.cpp
+++ Algo/algo.cpp
@@ -6,36 +6,25 @@
 #define MIN(X,Y) (X<Y?X:Y)
 using namespace std;
 vector<LL> seq;
+LL ans[10001][10001];
 int main(){
-    int n=100000000;
+    int n=10000;
     //cin>>n;
-    //generate sequence
     seq.push_back(0);
     for(int i=1;i<n;i++)
-        seq.push_back(((seq[i-1]*seq[i-1])%MOD+45)%MOD);
-
-    //simulate game
-    LL score=0;
-    int s=0;
-    int e=n-1;
-    while(s<=e){
-    	if(seq[s]>=seq[e]){
-		score+=seq[s];
-		s++;
-	}
-	else{
-		score+=seq[e];
-		e--;
-	}
-	if(s<=e){
-		if(seq[s]>=seq[e])
-			s++;
-		else
-		        e--;
-	}
-	if(score<0)
-		cout<<score<<endl;
-    }
-    cout<<"F(100000000) : "<<score<<endl;
+        seq.push_back((seq[i-1]*seq[i-1]+45)%MOD);
+    for(int i=0;i<n;i++)
+        ans[i][i]=seq[i];
+    for(int i=0;i+1<n;i++)
+        ans[i][i+1]=MAX(seq[i],seq[i+1]);
+    for(int l=3;l<=n;l++)
+        for(int s=0;s+l-1<n;s++){
+            int e=s+l-1;
+            LL a1,a2;
+            a1=seq[s]+MIN(ans[s+2][e],ans[s+1][e-1]);
+            a2=seq[e]+MIN(ans[s+1][e-1],ans[s][e-2]);
+            ans[s][e]=MAX(a1,a2);
+        }
+    cout<<ans[0][n-1]<<endl;
     return 0;
 }
